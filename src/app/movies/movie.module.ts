4f4bec9bf5af60b8b9df42f4599e5c3151b8b767
Ariful Islam <a.islam@stscapital.lt>
2021-10-26 03:04:32 +0600

    handson table setup

--- src/app/movies/movie.module.ts
+++ src/app/movies/movie.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HotTableModule } from '@handsontable/angular';
 import { MovieRoutingModule } from './movie-routing.module';
 import { MovieComponent } from './movie/movie.component';
 
 @NgModule({
   declarations: [MovieComponent],
-  imports: [MovieRoutingModule],
+  imports: [
+      MovieRoutingModule,
+      HotTableModule
+    ],
   providers: []
 })
 export class MovieModule {}
--- src/app/movies/movie/movie.component.ts
+++ src/app/movies/movie/movie.component.ts
@@ -1,11 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-
+import Handsontable from 'handsontable';
 @Component({
   selector: 'app-movie',
   templateUrl: './movie.component.html',
   styleUrls: ['./movie.component.scss']
 })
 export class MovieComponent implements OnInit {
+  dataset: any[] = [
+    { id: 1, name: 'Ted Right', address: 'Wall Street' },
+    { id: 2, name: 'Frank Honest', address: 'Pennsylvania Avenue' },
+    { id: 3, name: 'Joan Well', address: 'Broadway' },
+    { id: 4, name: 'Gail Polite', address: 'Bourbon Street' },
+    { id: 5, name: 'Michael Fair', address: 'Lombard Street' },
+    { id: 6, name: 'Mia Fair', address: 'Rodeo Drive' },
+    { id: 7, name: 'Cora Fair', address: 'Sunset Boulevard' },
+    { id: 8, name: 'Jack Right', address: 'Michigan Avenue' }
+  ];
+  hotSettings: Handsontable.GridSettings = {
+    data: this.dataset,
+    colHeaders: true,
+    stretchH: 'all',
+    autoWrapRow: true,
+    columnSorting: true,
+    fillHandle: false,
+    licenseKey: 'non-commercial-and-evaluation'
+  };
+
   constructor() {
     // do nothing
   }
